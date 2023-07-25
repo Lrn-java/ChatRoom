6b51fb8ea341dafe0bc30554538e2b7568bc5b56
Lrn-java <625514232@qq.com>
2023-07-25 16:09:22 +0800

    Initial commit

--- src/ChatRoom/li/LoginScreen/LoginScreen.java
+++ src/ChatRoom/li/LoginScreen/LoginScreen.java
@@ -1,9 +1,8 @@
 package ChatRoom.li.LoginScreen;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
-
-
 /**
  * 这个类的作用创建登录界面的属性
  * @author Lrn
@@ -24,11 +23,57 @@ public class LoginScreen extends JFrame {
     private final int getScreenHigh = screenSize.height;
 
 
+    private final JTextField jTextField = new JTextField();
+    private final JPasswordField jPasswordField = new JPasswordField();
+    private final Container container = getContentPane();
+
+    private final ImageIcon imageIcon = new ImageIcon("D:\\IDEA-Work\\ChatRoom\\src\\ChatRoom\\li\\Material\\登录界面素材.png");
+    private final JButton VisitorLogin = new JButton();
+    private final JButton QQ_Login = new JButton();
+
+
     public LoginScreen(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(wide,high);
         setLocation(getsX(),getsY());
+        setIconImage(null);
+        setBackground(new Color(0xFFFFFF));
 
+        setLayout(null);
+
+        //添加登录界面背景
+        JLabel jLabel = new JLabel(imageIcon);
+        jLabel.setBounds(0,0,400,530);
+        Integer minValue = Integer.MIN_VALUE;
+        getLayeredPane().add(jLabel,minValue);
+        JPanel jPanel = (JPanel) getContentPane();
+
+        Border border = BorderFactory.createMatteBorder(0,0,1,0,Color.BLACK);
+
+        jTextField.setText("QQ账号/游客账号");
+        jTextField.setBounds(440,250,200,30);
+        jTextField.setBorder(border);
+
+        jPasswordField.setText("密码");
+        jPasswordField.setBounds(440,300,200,30);
+        jPasswordField.setBorder(border);
+
+        VisitorLogin.setText("游客登录");
+        VisitorLogin.setBounds(553,340,85,30);
+
+        QQ_Login.setText("登 录");
+        QQ_Login.setBounds(440,340,85,30);
+
+
+        //添加组件
+        container.add(jTextField);
+        container.add(jPasswordField);
+        //添加按钮
+        container.add(VisitorLogin);
+        container.add(QQ_Login);
+
+        setResizable(false);
+        jPanel.setOpaque(false);
         setVisible(true);
     }
 
@@ -51,5 +96,6 @@ public class LoginScreen extends JFrame {
         //通过屏幕高度减去窗口高度并除以2，得到垂直位置
         return (this.getScreenHigh - high) / 2;
     }
+
 
 }
