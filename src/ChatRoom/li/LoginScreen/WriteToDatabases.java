53b3873e68afe0afd6082fb6fb99b26558ecb96e
Lrn-java <625514232@qq.com>
2023-07-28 17:03:19 +0800

    Initial commit

--- src/ChatRoom/li/LoginScreen/WriteToDatabases.java
+++ src/ChatRoom/li/LoginScreen/WriteToDatabases.java
@@ -18,14 +18,11 @@ public interface WriteToDatabases {
     String mysql_password = "";
     String databasesURL = "jdbc:mysql://*:3306/user_info";
 
-    default void getMassage(String iD, String usermessage, String password1){
+    default void getMassage(String iD, String usermessage, String password1) throws ClassNotFoundException,SQLException{
         //写入到user_message数据库中，这个数据库中有一个表，用来保存用户名和密码这些基本信息
-        try {
 
             //加载驱动
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-
 
             //创建连接
             Connection connection = DriverManager.getConnection(databasesURL,mysql_user,mysql_password);
@@ -38,15 +35,15 @@ public interface WriteToDatabases {
             int a = ps.executeUpdate();
 
             //抛出异常
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 
-    //随机生成ID
+    /**
+     * 生成一个6位的随机数，用与绑定ID
+     * @return 随机生成6位数
+     */
     default int random_ID(){
-        Random random = new Random();
-        return 100000 + random.nextInt(900000);
+        return 100000 + new Random().nextInt(900000);
     }
 
 
