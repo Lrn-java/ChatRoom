1890e53b23b0f01f6b57eac52e8bfb38d363b7ac
cjyddos <103885521+cjyddos@users.noreply.github.com>
2023-08-09 00:08:36 +0800

    完善加解密工具类

--- src/ChatRoom/li/unit/tesk.java
+++ src/ChatRoom/li/unit/tesk.java
@@ -1,22 +1,18 @@
 package ChatRoom.li.unit;
 
-import sun.java2d.pipe.AAShapePipe;
-
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class tesk {
-    public static void main(String[] args) {
-//        new EdcryptionArrLoad().EdcryptionArrLoad("ss","ll");
-        try {
-            byte[] encode = new EnAndDecode().encode("hahah");
-            System.out.println("加密："+Arrays.toString(encode));
-            String s = new String(encode);
-            System.out.println(s);
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws GeneralSecurityException {
+        String s = "sdl";
+        byte[] tyconversione = typeconversione.tyconversione(s);
+        System.out.println(Arrays.toString(tyconversione));
+
+        byte[] sdls = new EnAndDecode().encode(s);
+        System.out.println(Arrays.toString(sdls));
+        byte[] decode = new EnAndDecode().decode(sdls);
+        System.out.println(Arrays.toString(decode));
 
 
     }
---
+++ src/ChatRoom/li/unit/typeconversione.java
@@ -0,0 +1,16 @@
+package ChatRoom.li.unit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public  class typeconversione {
+    public static byte[] tyconversione(String ... code){
+        //传进来会是个string的arrlist
+        byte[] bytes = new byte[0];
+        for (String s : code) {
+            bytes = s.getBytes();
+
+        }
+        return bytes;
+    }
+}
