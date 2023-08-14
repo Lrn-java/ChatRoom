8eff38369a076999ddc5a269ce6dcdde714805df
Lrn-java <625514232@qq.com>
2023-08-14 17:28:52 +0800

    Initial commit

--- src/ChatRoom/li/Utils/EnAndDecode.java
+++ src/ChatRoom/li/Utils/EnAndDecode.java
@@ -1,13 +1,3 @@
-/*
-*
-*
-*
-* */
-
-
-
-
-
 package ChatRoom.li.Utils;
 import javax.crypto.*;
 import java.security.GeneralSecurityException;
@@ -15,19 +5,30 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
+/**
+ *
+ */
 public class EnAndDecode {
 
     private final String keyy = "key";
 
+    /**
+     * 字符串转换为字节数组
+     * @param code
+     * @return
+     */
     private  byte[] Type_conversioned(String code) {
         byte[] bytes;
-
-
         bytes = code.getBytes();
-
-
         return bytes;
     }
+
+    /**
+     * 生成密钥
+     * @param key
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
     private  SecretKey getkey(byte[] key) throws NoSuchAlgorithmException {
         SecureRandom sha1PRNG = SecureRandom.getInstance("SHA1PRNG");
         sha1PRNG.setSeed(key);
@@ -36,6 +37,13 @@ public class EnAndDecode {
         return aes.generateKey();
     }
 
+    /**
+     * 加密操作
+     * @param code
+     * @param key
+     * @return
+     * @throws java.security.GeneralSecurityException
+     */
     private  byte[] encodem(byte[] code, byte[] key) throws java.security.GeneralSecurityException {
         SecretKey getkey = getkey(key);
         Cipher aes = Cipher.getInstance("AES");
@@ -43,6 +51,17 @@ public class EnAndDecode {
         return aes.doFinal(code);
     }
 
+    /**
+     * 解密
+     * @param code
+     * @param key
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchPaddingException
+     * @throws IllegalBlockSizeException
+     * @throws BadPaddingException
+     * @throws InvalidKeyException
+     */
     private   byte[] decodem(byte[] code, byte[] key) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         SecretKey getkey = getkey(key);
         Cipher aes = Cipher.getInstance("AES");
@@ -50,35 +69,31 @@ public class EnAndDecode {
         return aes.doFinal(code);
     }
 
+    /**
+     * 字符串加密
+     * @param content
+     * @return
+     * @throws GeneralSecurityException
+     */
     public byte[] encode(String content) throws GeneralSecurityException {
-
-//        byte[] con = Type_conversion(content);
-//        byte[] key = Type_conversion(keyy);
-////        String string = Base64.getEncoder().encodeToString(content.getBytes());
-////        System.out.println();
-//        byte[] bytes = content.getBytes();
-//        System.out.println(Arrays.toString(bytes));
-////        byte[] bytes1 = key.getBytes();
-////        byte[] encodem = encodem(bytes, bytes1);
-////        System.out.println(Arrays.toString(encodem));
-////        byte[] decodem = decodem(encodem, bytes1);
-////        System.out.println(Arrays.toString(decodem));
-//        byte[] encodem = encodem(Type_conversion(content), Type_conversion(key));
-//        System.out.println(Arrays.toString(encodem));
-//        byte[] decodem = decodem(encodem, Type_conversion(key));
-//        System.out.println(Arrays.toString(decodem));
         byte[] kbytes = Type_conversioned(this.keyy);
         byte[] cbytes = Type_conversioned(content);
         return encodem(cbytes, kbytes);
-
-
     }
 
+    /**
+     *
+     * @param content
+     * @return
+     * @throws NoSuchPaddingException
+     * @throws IllegalBlockSizeException
+     * @throws NoSuchAlgorithmException
+     * @throws BadPaddingException
+     * @throws InvalidKeyException
+     */
     public byte[] decode(byte[] content) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         byte[] kbytes = Type_conversioned(this.keyy);
         return decodem(content, kbytes);
 
     }
-
-
 }
